6bc2bbe01c341a5a36db0d2232cee529a3cf2093
Hasnathasan <hasnatoooooooo@gmail.com>
2024-01-19 01:56:34 +0600

    Code formated

--- src/assets/components/Links/Links.jsx
+++ src/assets/components/Links/Links.jsx
@@ -1,13 +1,13 @@
+import React from "react";
 
-import React from 'react';
-
-const Links = ({route}) => {
-    return (
-            <li className='mb-5 md:mb-0'>
-                <a href={route.path} className="font-bold p-3">{route.name}</a>
-            </li>
-        
-    );
+const Links = ({ route }) => {
+  return (
+    <li className="mb-5 md:mb-0">
+      <a href={route.path} className="font-bold p-3">
+        {route.name}
+      </a>
+    </li>
+  );
 };
 
 export default Links;
